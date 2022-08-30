3e94635e9fb1f2d5d4e1ba82ca34fc0d5cbd574f
TheOje69 <82089114+TheOje69@users.noreply.github.com>
2022-08-30 14:19:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cdejvo6oxbk1.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jeffreychowsaiwai-employee"
+customregion = "us-east-1"
